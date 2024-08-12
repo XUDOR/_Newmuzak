741ae0a08367964c0e8d36e50b8387d4e0e8feb9
Roderick Shoolbraid <rdxenv@gmail.com>
2024-08-12 12:16:04 -0700

    simple  sript for looped dynamic content

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //++++++++++++++ script.js
+
+
+// Content arrays for Sections B and C
+const sectionBContent = [
+  "Post 1", "Post 2", "Post 3", "Post 4",
+  "Post 5", "Post 6", "Post 7", "Post 8",
+  "Post 9", "Post 10", "Post 11", "Post 12",
+  "Post 13", "Post 14", "Post 15", "Post 16"
+];
+
+const sectionCContent = [
+  "Article 1", "Article 2", "Article 3", "Article 4",
+  "Article 5", "Article 6", "Article 7", "Article 8",
+  "Article 9", "Article 10", "Article 11", "Article 12",
+  "Article 13", "Article 14", "Article 15", "Article 16"
+];
+
+// Function to generate grid items
+function generateGridContent(containerSelector, contentArray) {
+  const container = document.querySelector(containerSelector);
+  contentArray.forEach(item => {
+    const div = document.createElement('div');
+    div.className = 'square';
+    div.textContent = item;
+    container.appendChild(div);
+  });
+}
+
+// Generate content for Sections B and C
+generateGridContent('.B-grid', sectionBContent);
+generateGridContent('.C-grid', sectionCContent);
